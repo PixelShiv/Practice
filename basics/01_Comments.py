7d9b56ba78b56ee82eefb47f9fe355061fb3bbcc
Shivakumarp <kumarpshivap@gmail.com>
2024-10-09 21:30:22 +0530

    firstFile

---
+++ basics/01_Comments.py
@@ -0,0 +1,10 @@
+'''Comments:
+•	Are used to write the description about application logics to understand the logics easily.
+•	The main objective comments the code maintenance will become easy.
+•	The comments are non-Executable code.
+•	Python will ignore comments in run time.
+It has 2 types:
+1)	Single line comment. --  #SingleLineComment.
+2)	Multiline comment.-- ‘’’MultiLineComment’’’
+'''
+
