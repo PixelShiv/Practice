6b71fb6b85af9809e62e670e7c8a0c440d5891cf
PShivakumar <80328475+PixelShiv@users.noreply.github.com>
2025-09-26 22:49:24 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,14 @@
+package com.example;
+
+public class App {
+    private String name;
+
+    public App() {
+        this.name = "MyApp";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
